e4b56d60a5096aae21048a1510115c301a7e3e1d
Fatima Khamoor <fatima.hkhamoor@gmail.com>
2025-07-28 09:15:38 +0500

    Created _check_aliens_bottom() helper method to check alien reach the bottom.

--- alien_invasion.py
+++ alien_invasion.py
@@ -108,6 +108,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         # Decrement ships left.
@@ -123,6 +126,15 @@ class AlienInvasion:
 
         # Pause.
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
